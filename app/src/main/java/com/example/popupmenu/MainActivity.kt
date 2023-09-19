0e1f2b0c42aa56a07125a24f032087dd7727064c
unknown <Aprendiz@POPDFTCCSFSD101.FormacionSena.RED>
2023-09-19 17:14:01 -0500

    Trabajo Guststrap

---
+++ app/src/main/java/com/example/popupmenu/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.popupmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(ChatFragment())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(SettingFragment())
+                    true
+                }
+                else -> {
+                    // Manejar cualquier otro caso aquí, si es necesario
+                    false
+                }
+
+
+
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
+}
